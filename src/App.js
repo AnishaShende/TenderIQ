744fe4d993fa3383dd2802dd2c576f3a0a34823c
Atharva Atterkar <136713071+atharvaatterkar@users.noreply.github.com>
2025-02-19 15:48:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SearchResults from "./pages/SearchResults";
-import Home from "./pages/Home";
-import ResearchLaws from "./pages/ResearchLaws";
-import CaseSummaries from "./pages/Casesumm";
-import Insights from "./pages/Insights";
-import LegalBriefDrafting from "./pages/Briefdraft";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchResults from './pages/SearchResults';
+import Home from './pages/Home';
+import ResearchLaws from './pages/ResearchLaws'
+import CaseSummaries from './pages/Casesumm';
+import Insights from './pages/Insights';
+import LegalBriefDrafting from './pages/Briefdraft';
+import pdfchat from './pages/Chatpdf';
+
+
+
+
+
+
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState('');
   return (
-    <div className="font-sans antialiased">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/search-results"
-          element={<SearchResults searchQuery={searchQuery} />}
-        />
-        <Route path="/ResearchLaws" element={<ResearchLaws />} />
-        <Route path="/CaseSummaries" element={<CaseSummaries />} />
-        <Route path="/Insights" element={<Insights />} />
-        <Route path="/Briefdraft" element={<LegalBriefDrafting />} />
-      </Routes>
-    </div>
+    
+      <div className="font-sans antialiased">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search-results" element={<SearchResults searchQuery={searchQuery} />} />
+          <Route path="/ResearchLaws" element={<ResearchLaws />} />
+          <Route path="/CaseSummaries" element={<CaseSummaries />} />
+          <Route path="/Insights" element={<Insights />} />
+          <Route path="/Briefdraft" element={<LegalBriefDrafting />} />
+          <Route path="/Chatpdf" element={<pdfchat />} />
+        </Routes>
+      </div>
+    
   );
 }
 
