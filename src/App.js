00bd6704575122f6c31f42e80897a6f1a7333f94
Anisha Shende <anishashende963@gmail.com>
2025-02-19 14:11:03 +0530

    fix route

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchResults from './pages/SearchResults';
-import Home from './pages/Home';
-import ResearchLaws from './pages/ResearchLaws'
-import CaseSummaries from './pages/Casesumm';
-import Insights from './pages/Insights';
-import LegalBriefDrafting from './pages/Briefdraft';
-
-
-
-
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SearchResults from "./pages/SearchResults";
+import Home from "./pages/Home";
+import ResearchLaws from "./pages/ResearchLaws";
+import CaseSummaries from "./pages/Casesumm";
+import Insights from "./pages/Insights";
+import LegalBriefDrafting from "./pages/Briefdraft";
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   return (
-    
-      <div className="font-sans antialiased">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search-results" element={<SearchResults searchQuery={searchQuery} />} />
-          <Route path="/ResearchLaws" element={<ResearchLaws />} />
-          <Route path="/CaseSummaries" element={<CaseSummaries />} />
-          <Route path="/Insights" element={<Insights />} />
-        <Route path="/Briefdraft" element={<LegalBriefDrafting />} />
-        <Route path="/"
-        </Routes>
-      </div>
-    
+    <div className="font-sans antialiased">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/search-results"
+          element={<SearchResults searchQuery={searchQuery} />}
+        />
+        <Route path="/ResearchLaws" element={<ResearchLaws />} />
+        <Route path="/CaseSummaries" element={<CaseSummaries />} />
+        <Route path="/Insights" element={<Insights />} />
+        <Route path="/Briefdraft" element={<LegalBriefDrafting />} />z
+      </Routes>
+    </div>
   );
 }
 
